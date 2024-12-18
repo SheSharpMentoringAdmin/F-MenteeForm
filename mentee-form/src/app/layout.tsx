b07db52c8cb6a777c88560a66254911468875e88
SheSharpMentoringAdmin <mentoring@shesharp.org.nz>
2024-12-18 13:30:26 +1300

    add suppressHydrationWarning

--- mentee-form/src/app/layout.tsx
+++ mentee-form/src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body>
+      <body suppressHydrationWarning>
         <Provider store={store}>{children}</Provider>
       </body>
     </html>
